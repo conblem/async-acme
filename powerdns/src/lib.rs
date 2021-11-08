5d4f698216a6ac8631b35368ba7c508ba64f0b0a
conblem <mail@conblem.me>
2021-11-08 22:40:59 +0100

    fix: wip

--- powerdns/src/lib.rs
+++ powerdns/src/lib.rs
@@ -28,29 +28,34 @@ pub fn powerdns_container<T: ToString, M: Into<String>>(
 ) -> Container<'_, clients::Cli, GenericImage> {
     let wait_for = WaitFor::message_on_stderr("Creating backend connection for TCP");
 
-    let powerdns = GenericImage::new("psitrax/powerdns")
+    let powerdns = GenericImage::new("powerdns:latest")
         .with_wait_for(wait_for)
         .with_env_var("MYSQL_HOST", mysql)
         .with_env_var("MYSQL_DB", "asyncacme");
 
-    let free_port = portpicker::pick_unused_port().expect("No port found");
     let run_args = RunArgs::default()
         .with_network("asyncacme")
-        .with_name(name)
-        .with_mapped_port((free_port, 8081));
+        .with_name(name);
 
     docker.run_with_args(powerdns, run_args)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::error::Error;
+
     use mysql::mysql_container;
+
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn works() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         let docker = clients::Cli::default();
         let _mysql = mysql_container(&docker, "mysql-powerdns");
         let powerdns = powerdns_container(&docker, "powerdns", "mysql-powerdns");
-        let powerdns_port = powerdns.get_host_port(8081).unwrap();
+        let powerdns_port = powerdns.get_host_port(8081).ok_or("Port not found")?;
+        std::thread::sleep(std::time::Duration::from_secs(20));
+
+        Ok(())
     }
 }
